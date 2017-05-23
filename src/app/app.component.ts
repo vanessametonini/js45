c26a230ab9f000872aeadee8c94086014b4974f0
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-05-23 15:43:35 -0300

    Exercícios ate cap03

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http'
+import { FotoComponent } from './foto/foto.component'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  title = 'Caelum Pic!';
+  fotos: FotoComponent[] = [];
+
+  constructor(http: Http){
+      http.get('http://localhost:3000/v1/fotos')
+          .map(
+            res => res.json()
+          )
+          .subscribe(
+            (fotos) => {
+              this.fotos = fotos
+            },
+            (erro) => {
+              console.log(erro)
+            }
+          )
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+import 'rxjs/add/operator/map'
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import  { FotoModule } from './foto/foto.module'
 
 @NgModule({
   declarations: [
@@ -12,7 +16,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FotoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
